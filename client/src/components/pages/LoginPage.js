30bc99ef35d8eb75ed5e2ab627da2c001756e547
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-03-18 17:44:27 +0300

    Classes to hooks
    Turning all the classes to functions with hooks

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,50 +1,44 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-class LoginForm extends React.Component {
-    render() {
-        return (
-            <form>
-                <div>
-                    <label for="email">Email</label>
-                    <input
-                        name="email"
-                        type="email"
-                        placeholder="Enter Email"
-                    />
-                </div>
-                <div>
-                    <label for="password">Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Enter Password"
-                    />
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Login</button>
-            </form>
-        )
-    }
-}
-
-class RegisterLink extends React.Component {
-    render() {
-        return (
-            <Link to="/register">Register</Link>
-        )
-    }
-}
-
-class LoginPage extends React.Component {
-    render() {
-        return (
+const LoginForm = () => {
+    return (
+        <form>
             <div>
-                <h1>Login</h1>
-                <LoginForm/>
-                <RegisterLink/>
+                <label for="email">Email</label>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email"
+                />
             </div>
-        )
-    }
+            <div>
+                <label for="password">Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Enter Password"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
+        </form>
+    )
+}
+
+const RegisterLink = () => {
+    return (
+        <Link to="/register">Register</Link>
+    )
+}
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+            <RegisterLink/>
+        </div>
+    )
 }
 
 export default LoginPage
--- client/src/components/pages/RegisterPage.js
+++ client/src/components/pages/RegisterPage.js
@@ -1,68 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class RegisterForm extends React.Component {
-    render() {
-        return (
-            <form>
-                <div className="name-form">
-                    <label for="name">Name</label>
-                    <input
-                        name="name"
-                        type="name"
-                        placeholder="Enter Name"
-                    />
-                </div>
-                <div className="email-form">
-                    <label for="email">Email</label>
-                    <input
-                        name="email"
-                        type="email"
-                        placeholder="Enter Email"
-                    />
-                </div>
-                <div className="password-form">
-                    <label for="password">Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Enter password"
-                    />
-                </div>
-                <div className="password-form">
-                    <label  for="password2">Confirm Password</label>
-                    <input
-                        name="passwrod2"
-                        type="password"
-                        placeholder="Confirm Password"
-                    />
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">
-                    Register
-                </button>
-            </form>
-        )
-    }
-}
-
-class LoginLink extends React.Component {
-    render() {
-        return (
-            <Link to="users/login">Login</Link>
-        )
-    }
-}
-
-class RegisterPage extends React.Component {
-    render() {
-        return (
-            <div className="register-page">
-                <h1>Register</h1>
-                <RegisterForm/>
-                <p>Have an account? <LoginLink/></p>
+const RegisterForm = () => {
+    return (
+        <form>
+            <div className="name-form">
+                <label for="name">Name</label>
+                <input
+                    name="name"
+                    type="name"
+                    placeholder="Enter Name"
+                />
             </div>
-        )
-    }
+            <div className="email-form">
+                <label for="email">Email</label>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email"
+                />
+            </div>
+            <div className="password-form">
+                <label for="password">Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Enter password"
+                />
+            </div>
+            <div className="password-form">
+                <label  for="password2">Confirm Password</label>
+                <input
+                    name="password2"
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">
+                Register
+            </button>
+        </form>
+    )
+}
+
+const LoginLink = () => {
+    return (
+        <Link to="users/login">Login</Link>
+    )
+}
+
+const RegisterPage = () => {
+    return (
+        <div className="register-page">
+            <h1>Register</h1>
+            <RegisterForm/>
+            <p>Have an account? <LoginLink/></p>
+        </div>
+    )
 }
 
 export default RegisterPage
