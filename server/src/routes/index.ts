82e9e7aaaf55f724819ad2d6b039ec9cc59858c4
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-03-22 19:02:32 +0300

    Fetching from server on server
    Installed node-fetch
    
    Updated routes/index to export router and made it send json data
    
    changed app.ts so it fetches from "/test"

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,15 @@
 const express = require("express")
 const router = express.Router()
-import {ensureAuthenticated} from "../config/auth"
+// import {ensureAuthenticated} from "../config/auth"
 
-router.get('/register')
-router.post('/register')
+//router.get('/register')
+//router.post('/register')
 
-router.get('/login')
-router.post('/login')
+//router.get('/login')
+//router.post('/login')
+
+router.get('/test', (req, res) => {
+    res.json({data: "hello"})
+})
+
+module.exports = router
