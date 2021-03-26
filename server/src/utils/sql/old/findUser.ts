2b745c1a8c0a9f8079b1704b395bd6cf56cc7fe1
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-03-26 18:25:04 +0300

    Moving old functions
    Moving the old SQL related functions into a new directory for clarity.

--- server/src/utils/sql/old/findUser.ts
+++ server/src/utils/sql/old/findUser.ts
@@ -1,9 +1,9 @@
-import con from "./myCon"
-import * as types from "../types"
+import con from "../myCon"
+import * as types from "../../types"
 
 const findUser = (socketId): Promise<types.connection>  => new Promise((resolve, reject) =>{
     let username = ''
-    let room = ''
+    let roomId = ''
     con.connect(error => {
         const selectUser = 'SELECT userId, room FROM sockets WHERE socketId = ? LIMIT 1'
         const userData = 'SELECT username FROM users WHERE id = ? LIMIT 1'
@@ -11,12 +11,12 @@ const findUser = (socketId): Promise<types.connection>  => new Promise((resolve,
         con.query(selectUser, [socketId], (err, res) => {
             if (err) return reject(err)
             if (res[0]) {
-                room = res[0].room
+                roomId = res[0].room
                 con.query(userData, [res[0].userId], (err, res) => {
                     if (err) return reject(err)
                     username = res[0].username
-                    console.log(username, room)
-                    return resolve({socketId, username, room})                   
+                    console.log(username, roomId)
+                    return resolve({socketId, username, roomId})                   
                 })
             }
         })
