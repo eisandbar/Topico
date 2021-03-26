e963ed763c355cec667d11ebfcc7641039832e88
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-03-26 18:23:18 +0300

    findOne
    this function now searches based on any of the parameters a user can have (other than password).
    
    Not sure if this works, or if I will have to split into several functions.

--- server/src/utils/sql/findOne.ts
+++ server/src/utils/sql/findOne.ts
@@ -1,16 +1,35 @@
 import con from "./myCon"
 import * as types from "../types"
 
-const findOne = async (email:string) : Promise<types.user> => new Promise((resolve, reject) => {
+const findOne = async (user: types.user) : Promise<types.user> => new Promise((resolve, reject) => {
     con.connect(error => {
-        const findUser = "SELECT username, password FROM users WHERE email = ? LIMIT 1"
-        con.query(findUser, [[[email]]], (err, res) => {
+        let key = ""
+        let value = ""
+        if (user.username) {
+            key = "username"
+            value = user.username
+        } else if (user.email) {
+            key = "email"
+            value = user.email
+        } else if (user.id) {
+            key = "id"
+            value = `${user.id}`
+        }else {
+            return resolve(null)
+        }
+
+        const findUser = `SELECT * FROM users WHERE ${key} = ${value} LIMIT 1`
+        con.query(findUser, (err, res) => {
             if (err) return reject(err)
-            return resolve({
-                username: res[0].username,
-                password: res[0].password,
-                email,
-            })
+            if (res[0]) {
+                return resolve({
+                    id: res[0].id,
+                    username: res[0].username,
+                    password: res[0].password,
+                    email: res[0].email,
+                })
+            } else return resolve(null)
+            
         })
 
     })
