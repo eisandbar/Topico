7dd0653e2f72b7e944acae52ad7b910b7377ad79
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-03-29 22:53:17 +0300

    Create sendGet.js
    Reused code from sendPost, helps with clarity, might join into single function later

---
+++ client/src/utils/sendGet.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch"
+
+/* 
+    sendGet function GETs from the server.
+    Returns a Promise that resolves to the parsed JSON object.
+*/
+
+export const sendGet = async ({url}) => new Promise((resolve, reject) => {
+    fetch("http://localhost:3000" + url, { // GET from server
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+    })
+    .then(res => res.json())
+    .then(resJson => {
+    console.log(JSON.parse(resJson))
+    return resolve(JSON.parse(resJson))
+    })
+})
