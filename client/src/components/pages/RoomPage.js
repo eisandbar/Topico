42eef616e1c2b947cfaa4c446b9c14a0bb4c37c6
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-03-31 22:00:47 +0300

    Room page styling changes
    Added styling to the room page. Still unfinished

--- client/src/components/pages/RoomPage.js
+++ client/src/components/pages/RoomPage.js
@@ -1,10 +1,14 @@
 import React from "react"
 
+
+
 import RoomContainer from "../chat_parts/RoomContainer"
 import Profile from "../chat_parts/Profile"
 import { useRooms } from "../hooks/useRooms"
 import { useAuth } from "../auth/ProvideAuth"
 import { useUser } from "../user/ProvideUser"
+
+import '../../sass/roomPage.scss'
 
 /* 
     Room page component.
@@ -18,14 +22,41 @@ const RoomPage = (props) => {
     const {user} = useUser()
     
     return (
-        <div className="full-height">
-            <div className="nav-column">
+        <div className="fullheight">
+            <div className="columns fullheight is-gapless is-mobile">
+
+                <div className="column is-3 is-narrow-mobile">
+                    <div className="rows fullheight">
+
+                        <div className="row profile flex-column">
+                            <div className="box" />
+                            <Profile username={user.username} />
+                            <button onClick={auth.signout} > Logout </button>
+                        </div>
+
+                        <div className="row side-bar">
+                            <div className="box" />
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="column is-9 main-content">
+
+                    <div className="box" />
+                    <RoomContainer rooms={rooms} />
+
+                </div>
+
+            </div>
+        
+            {/* <div className="nav-column">
                 <Profile username={user.username} />
                 <button onClick={auth.signout} > Logout </button>
             </div>
             <div className="content-column">
                 <RoomContainer rooms={rooms} />
-            </div>
+            </div> */}
         </div>
     )
 }
