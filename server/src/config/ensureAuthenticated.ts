28dc0ffe94d7b9ebf4a5526b7267bf9f22992997
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-04-06 18:25:59 +0300

    ensureAuthenticated returns username
    Returns username, which is lost after refresh on the client

--- server/src/config/ensureAuthenticated.ts
+++ server/src/config/ensureAuthenticated.ts
@@ -1,6 +1,9 @@
 export const ensureAuthenticated = (req: any, res: any, next: any) => {
         if(req.isAuthenticated){
-            res.json(JSON.stringify({loggedIn : true}))
+            res.json(JSON.stringify({
+                loggedIn : true,
+                username: req.user.username
+            }))
         } else {
             res.json(JSON.stringify({loggedIn : false}))
         }
