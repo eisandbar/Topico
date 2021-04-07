759a07ab9f72d5a25e0a2dedab2024ec6d0debf8
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-04-07 03:48:54 +0300

    isAuthentcated not working
    isAuthenticated doesn't seem to work when trying to access a restricted page without logging in.
    It returns true even when it shouldn't. Could be because of typing issues.
    
    Changed ensureAuthenticated to simply check if req.user is defined, seems to fix the problem.
    
    Also only returns next() after successful authentication.

--- server/src/config/ensureAuthenticated.ts
+++ server/src/config/ensureAuthenticated.ts
@@ -1,11 +1,11 @@
 export const ensureAuthenticated = (req: any, res: any, next: any) => {
-    if (req.isAuthenticated) {
+    if (req.user) {
         res.json(JSON.stringify({
             loggedIn: true,
             username: req.user.username
         }))
+        return next()
     } else {
         res.json(JSON.stringify({ loggedIn: false }))
     }
-    next()
 }
