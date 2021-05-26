fe7d20646507c3ae0b91e20e1e1d3cfa809ea9a8
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-05-26 15:08:57 +0300

    Check for empty roomname
    Now checks for empty strings so that they are ignored immediately

--- server/src/utils/sql/newRoom.ts
+++ server/src/utils/sql/newRoom.ts
@@ -2,6 +2,11 @@ import con from './myCon'
 import * as types from '../types'
 
 const newRoom = async (roomname: string) : Promise<number> => new Promise((resolve, reject) => {
+    if (roomname == "") {
+        console.log("Room name cannot be null")
+        return reject()
+    }
+    
     con.connect( error => {
         const addRoom = "INSERT INTO rooms (roomname) VALUES ?"
         con.query(addRoom, [[[roomname]]], (err, res) => {
